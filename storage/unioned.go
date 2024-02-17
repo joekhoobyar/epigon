c12015adbb9c18f36f156857c4ef3b51dc2a8133
Joe Khoobyar <fourheads@gmail.com>
2024-02-16 23:17:54 -0500

    unioned storage tolerates missing prefix in one but not two layers

--- storage/unioned.go
+++ storage/unioned.go
@@ -87,15 +87,16 @@ func (u *UnionedCache) List(location string) (subkeys []string, err error) {
 			subkeys = r.(collectionRecord).subkeys
 		}
 	} else {
-		var basekeys, tempkeys []string
+		basekeys, baseerr := u.base.List(location)
+		tempkeys, temperr := u.temp.List(location)
 
-		if basekeys, err = u.base.List(location); err == nil {
-			if tempkeys, err = u.temp.List(location); err == nil {
-				subkeys = make([]string, 0, len(basekeys)+len(tempkeys))
-				subkeys = append(subkeys, basekeys...)
-				subkeys = append(subkeys, tempkeys...)
-				u.cache[location] = collectionRecord{subkeys: subkeys}
-			}
+		if (baseerr == nil && (temperr == nil || IsPrefixNotFound(temperr))) ||
+			(temperr == nil && (baseerr == nil || IsPrefixNotFound(baseerr))) {
+
+			subkeys = make([]string, 0, len(basekeys)+len(tempkeys))
+			subkeys = append(subkeys, basekeys...)
+			subkeys = append(subkeys, tempkeys...)
+			u.cache[location] = collectionRecord{subkeys: subkeys}
 		}
 	}
 	return
