3a4eb010ecfab4a6c82fd564d70b9dede3991470
Joe Khoobyar <fourheads@gmail.com>
2024-02-16 21:18:43 -0500

    resource location

--- rest/http.go
+++ rest/http.go
@@ -5,8 +5,13 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"regexp"
 
 	"github.com/julienschmidt/httprouter"
+)
+
+var (
+	routerParamRegexp = regexp.MustCompile(":[^/]+")
 )
 
 type Options struct {
@@ -31,6 +36,20 @@ func (srv *Server) defaultNotFound(w http.ResponseWriter, rq *http.Request) {
 		w.WriteHeader(599)
 	}
 	w.Write([]byte(fmt.Sprint("No route matches", m, p))) // nolint
+}
+
+func LocateResource(route string, params httprouter.Params) (location string, err error) {
+	route = routerParamRegexp.ReplaceAllStringFunc(route, func(match string) string {
+		value := params.ByName(match[1:])
+		if value == "" {
+			err = fmt.Errorf("LocateResource: %s: no such path parameter", match)
+		}
+		return value
+	})
+	if err == nil {
+		location = route
+	}
+	return
 }
 
 func buildServer(options Options) *Server {
